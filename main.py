b5887751cd37efb3cf884830c8101c0453d014c5
jyh9763 <jyh976320@gmail.com>
2025-07-23 15:38:48 +0900

    날짜 클릭하면 약속 기입 페이지로 이동

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -12,6 +12,12 @@ app.mount("/static/", StaticFiles(directory="static"), name="static")
 # templates 파일
 templates = Jinja2Templates(directory="templates")
 
+# main page
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+# metting page
+@app.get("/meet", response_class=HTMLResponse)
+async def meet_root(request: Request, day: int):
+    return templates.TemplateResponse("meet.html",{"request": request, "day": day})
