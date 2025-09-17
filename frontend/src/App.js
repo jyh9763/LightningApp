14b2ea21434c2f56323ceb96ad12ca1f72469602
jyh9763 <jyh976320@gmail.com>
2025-09-17 18:05:54 +0900

    update: import Login.js to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from "react";
 import axios from "axios";
+import Login from "./components/Login"
 
 function App() {
   // 첫 페이지
   const [page, setPage] = useState("login");
-
-  // 로그인 변수
-  const [loginId, setLoginId] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
 
   // 회원가입 변수
   const [regId, setRegId] = useState("");
@@ -19,30 +15,6 @@ function App() {
 
   function handleChangePage(page){
     setPage(page);
-  }
-
-  // 로그인 함수
-  function handleSignup() {
-    // Spring Boot 호출 및 연동
-    const data = {
-      userId: loginId,
-      userPassword: loginPassword
-    };
-
-    axios
-      .post("http://localhost:8080/api/users/login", data)
-      .then((response) => {
-        if (response.data === "main") {
-          alert("로그인에 성공했습니다.");
-          setPage("register");
-        } else {
-          alert("로그인에 실패했습니다.");
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("로그인 오류가 발생하였습니다.");
-      })
   }
 
   // 회원가입 함수
@@ -71,52 +43,8 @@ function App() {
       <header className="App-header">
         <h1>{page === "register" ? "회원가입" : "어서오세요"}</h1>
 
-        {/* 로그인 */}
-        {page === "login" && (
-            <div className="login-box">
-                <div className="input-group">
-                    <label htmlFor="login_id">아이디</label>
-                    <input
-                      type="text"
-                      id="login_id"
-                      placeholder="Id"
-                      className="input-field"
-                      value={loginId}
-                      onChange={(e) => setLoginId(e.target.value)}
-                    />
-                </div>
-
-                <div className="input-group">
-                    <label htmlFor="login_password">비밀번호</label>
-                    <input
-                      type="password"
-                      id="login_password"
-                      placeholder="Password"
-                      className="input-field"
-                      value={loginPassword}
-                      onChange={(e) => setLoginPassword(e.target.value)}
-                    />
-                </div>
-
-                <div className="button-group">
-                    {/* 로그인 버튼 클릭 시 메인 페이지로 이동*/}
-                    <button
-                      className="login-button"
-                      onClick={handleSignup}
-                    >
-                      Login
-                    </button>
-
-                    {/* 버튼 클릭 시 회원가입 페이지로 이동 */}
-                    <button
-                      className="register-button"
-                      onClick={() => setPage("register")}
-                    >
-                      회원가입
-                    </button>
-                </div>
-            </div>
-        )}
+          {/* 로그인 */}
+          {page === "login" && <Login setPage={setPage}/>}
 
         {/* 회원가입 */}
         {page === "register" && (
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,76 @@
+import React, {useState} from 'react';
+import axios from "axios";
+export default Login;
+
+function Login({setPage}){
+    const [loginId, setLoginId] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+
+    function handleLogin(){
+        const data = {
+            userId: loginId,
+            userPassword: loginPassword
+        };
+
+        axios
+            .post("http://localhost:8080/api/users/login", data)
+            .then((response) => {
+                if(response.data === "main"){
+                    alert("로그인에 성공하였습니다.");
+                    setPage("main");
+                } else{
+                    alert("로그인에 실패하였습니다.");
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                alert("로그인 오류가 발생하였습니다.");
+            })
+    }
+
+    return (
+        <div className="login-box">
+            <div className="input-group">
+                <label htmlFor="login_id">아이디</label>
+                <input
+                    type="text"
+                    id="login_id"
+                    placeholder="Id"
+                    className="input-field"
+                    value={loginId}
+                    onChange={(e) => setLoginId(e.target.value)}
+                />
+            </div>
+
+            <div className="input-group">
+                <label htmlFor="login_password">비밀번호</label>
+                <input
+                    type="password"
+                    id="login_password"
+                    placeholder="Password"
+                    className="input-field"
+                    value={loginPassword}
+                    onChange={(e) => setLoginPassword(e.target.value)}
+                />
+            </div>
+
+            <div className="button-group">
+                {/* 로그인 버튼 클릭 시 메인 페이지로 이동*/}
+                <button
+                    className="login-button"
+                    onClick={handleLogin}
+                >
+                    Login
+                </button>
+
+                {/* 버튼 클릭 시 회원가입 페이지로 이동 */}
+                <button
+                    className="register-button"
+                    onClick={() => setPage("register")}
+                >
+                    회원가입
+                </button>
+            </div>
+        </div>
+    );
+}
